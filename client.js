320855cef307db2c98548d86c554aac6a747ca47
Anmol Srivastava <anmol@Anmols-MacBook-Air.local>
2023-11-09 14:05:19 +0530

    Testing3 : Server-Client connection

--- client.js
+++ client.js
@@ -12,10 +12,28 @@ const dht = new HyperDHT()
 const conn = dht.connect(publicKey)
 conn.once('open', () => console.log('got connection!'))
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  rl.question('Enter your name: ', (name) => {
+    console.log(`Welcome, ${name}! You can start chatting.`);
+  
+    rl.on('line', (message) => {
+      // Send the message to the DHT using Hypercore
+      const messageObj = { user: name, message };
+      const messageString = JSON.stringify(messageObj);
+      core.append(messageString, () => {
+        console.log(`You: ${message}`);
+      });
+    });
+  });
+  
 //incoming messages
 core.createReadStream({ live: true }).on('data', (data) => {
     const { user, message } = JSON.parse(data.toString());
-    // console.log(`${user}: ${message}`);
+    console.log(`${user}: ${message}`);
   });
 
 process.stdin.pipe(conn).pipe(process.stdout)
