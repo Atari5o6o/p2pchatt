0924d497b7c4d8d59113e63ff26055e25b3c91f9
Anmol Srivastava <anmol@Anmols-MacBook-Air.local>
2023-11-09 14:30:36 +0530

    bug fixing3: Appending messages on new line

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const HyperDHT = require("hyperdht");
 const b4a = require("b4a")
 const hypercore = require('hypercore');
 const readline = require('readline');
+const fs = require('fs');
 
 console.log('Connecting to:', process.argv[2])
 const publicKey = b4a.from(process.argv[2], 'hex')
@@ -13,32 +14,45 @@ const conn = dht.connect(publicKey)
 conn.once('open', () => console.log('got connection!'))
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  
-  rl.question('Enter your name: ', (name) => {
-    console.log(`Welcome, ${name}! You can start chatting.`);
-  
-    // Create a writable stream for the Hypercore feed
-    const feed = core.createWriteStream();
-  
-    rl.on('line', (message) => {
-      // Send the message to the DHT using Hypercore
-      const messageObj = { user: name, message };
-      const messageString = JSON.stringify(messageObj);
-  
-      // Append the message to the Hypercore feed
-      feed.write(messageString + '\n', 'utf-8', () => {
-        console.log(`You: ${message}`);
-      });
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Enter your name: ', (name) => {
+  console.log(`Welcome, ${name}! You can start chatting.`);
+
+  // Create a writable stream for the Hypercore feed
+  const feed = core.createWriteStream();
+
+  // Create a readable stream for the Hypercore feed
+  const readStream = core.createReadStream({ live: true });
+
+  // Create a writable stream for saving messages to a file
+  const fileStream = fs.createWriteStream('chat-data.txt', { flags: 'a' });
+
+  rl.on('line', (message) => {
+    // Send the message to the DHT using Hypercore
+    const messageObj = { user: name, message };
+    const messageString = JSON.stringify(messageObj);
+
+    // Append the message to the Hypercore feed
+    feed.write(messageString + '\n', 'utf-8', () => {
+      console.log(`You: ${message}`);
     });
-  });
-  
-//incoming messages
-core.createReadStream({ live: true }).on('data', (data) => {
-    const { user, message } = JSON.parse(data.toString());
-    console.log(`${user}: ${message}\n`);
+
+    // Append the message to the file
+    fileStream.write(`${name}: ${message}\n`);
   });
 
-process.stdin.pipe(conn).pipe(process.stdout)
+  // Incoming messages
+  readStream.on('data', (data) => {
+    const { user, message } = JSON.parse(data.toString());
+    console.log(`${user}: ${message}\n`);
+
+    // Append the received message to the file
+    fileStream.write(`${user}: ${message}\n`);
+  });
+});
+
+// Pipe stdin, DHT connection, and stdout
+process.stdin.pipe(conn).pipe(process.stdout);
